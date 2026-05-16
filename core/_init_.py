17eac2b996d42a02e4948d7fea5ad84f179f59f4
NDTVC-AC.KE <roygichira084@gmail.com>
2026-05-16 16:31:12 +0300

    Rename BypassEngine to EvaderEngine in core module

--- core/_init_.py
+++ core/_init_.py
@@ -1,8 +1,21 @@
-# Core module
+# core/__init__.py
+# EnvKiller Core Module
+# MADE BY XSPEEN
+
 from .engine import EnvKillerEngine
 from .scanner import SecretScanner
 from .worm import WormCrawler
 from .port_scanner import PortScanner
 from .subdomain import SubdomainFinder
 from .dir_brute import DirBruteForcer
-from .evader import BypassEngine
+from .evader import EvaderEngine
+
+__all__ = [
+    'EnvKillerEngine',
+    'SecretScanner',
+    'WormCrawler',
+    'PortScanner',
+    'SubdomainFinder',
+    'DirBruteForcer',
+    'EvaderEngine'
+]
